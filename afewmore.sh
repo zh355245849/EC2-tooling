0e5cb6d75f2be8e99de7e32a958171cc2b5bbf80
Yangmu Jiang <celsius.j@gmail.com>
2017-04-16 21:04:43 -0400

    deleted ssh key file code, added ssh config check

--- afewmore.sh
+++ afewmore.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# TODO: remove PEM_FILE && ssh -i, add ssh config verification
 # TODO: make all path canonical
 
 ##### script configuration #####
@@ -9,7 +8,6 @@ PROG_NAME=$(basename "$0")
 CONFIG_DIR="$HOME/.afewmore"
 
 TASK_FILE="$CONFIG_DIR/task.0"
-PEM_FILE="us-east-1"
 
 COPY_NUM=1
 COPY_DIR="/data"
@@ -75,7 +73,19 @@ verify_aws() {
         fatal "aws not configured"
     fi
 }
-# verify_ssh_login()
+verify_ssh() {
+    local _origin_id="$1"
+    local _ssh_loc=$(which ssh)
+    if [ "$_ssh_loc" == "" ] ; then
+        fatal "ssh not installed"
+    fi
+    local _host=$(util_get_host $_origin_id "(origin)")
+    if [ "$_host" == "" ]; then exit 1; fi
+    local _login_test=$(ssh -o BatchMode=yes -o StrictHostKeyChecking=no $USER@$_host "echo Y" 2>/dev/null)
+    if [ "$_login_test" != "Y" ] ; then
+        fatal "ssh not properly configured"
+    fi
+}
 verify_copy_num() {
     local re='^[1-9][0-9]*$'
     if ! [[ "$1" =~ $re ]] ; then
@@ -84,24 +94,29 @@ verify_copy_num() {
 }
 verify_instance() {
     local _instance_id="$1"
-    local _msg="$2"
     local re='^i-[0-9a-f]+$'
     if ! [[ "$_instance_id" =~ $re ]] ; then
-        fatal "$_msg"
+        fatal "illegal instance id $_instance_id"
     fi
 }
 verify_copy_dir() {
-    local _ssh_key="$1"
-    local _origin_id="$2"
-    local _user="$3"
-    local _host="$4"
-    local _dir="$5"
+    local _origin_id="$1"
+    local _dir="$2"
+
     if [ "$_dir" == "" ] ; then
         fatal "directory can't be empty"
     fi
-    local _dir_exist=$(ssh -o BatchMode=yes -o StrictHostKeyChecking=no -i $_ssh_key $_user@$_host "if [ -d '$_dir' ] ; then echo T; else echo F; fi")
+
+    # check if origin instance has directory
+    local _host=$(util_get_host $_origin_id "(origin)")
+    if [ "$_host" == "" ]; then exit 1; fi
+    (do_check_ready "$_origin_id" "(origin)")
+    if [ "$?" != "0" ]; then exit 1; fi
+    local _user=$(util_get_user $_host "(origin)")
+    if [ "$_user" == "" ]; then exit 1; fi
+    local _dir_exist=$(ssh -o BatchMode=yes -o StrictHostKeyChecking=no $_user@$_host "if [ -d '$_dir' ] ; then echo T; else echo F; fi")
     if [ "$_dir_exist" != "T" ] ; then
-        fatal "can't find directory '$_dir' on origin instance ($_origin_id)"
+        fatal "can't find directory '$_dir' on instance $_origin_id (origin)"
     fi
 }
 
@@ -265,8 +280,8 @@ when_instance_done_end() {
 ##### Main Routines #####
 
 util_get_host() {
-    local _instance_id=$1
-    local _extra_msg=$2
+    local _instance_id="$1"
+    local _extra_msg="$2"
     local _host=$(aws ec2 describe-instances --instance-ids $_instance_id --output text --query "Reservations[*].Instances[*].PublicDnsName")
     if [ "$_host" == "" ] ; then
         fatal "can't find host of instance $_instance_id $_extra_msg"
@@ -275,14 +290,14 @@ util_get_host() {
     fi
 }
 util_get_user() {
-    local _host=$1
-    local _ssh_key=$2
-    local _user=$(ssh -o BatchMode=yes -o StrictHostKeyChecking=no -i $_ssh_key root@$_host "whoami" 2>/dev/null)
+    local _host="$1"
+    local _extra_msg="$2"
+    local _user=$(ssh -o BatchMode=yes -o StrictHostKeyChecking=no root@$_host "whoami" 2>/dev/null)
     if [ "$_user" != "root" ] ; then
         _user=$(echo $_user | sed -r 's/^[^"]*"([^"]+)".*$/\1/')
     fi
     if [ "$_user" == "" ] ; then
-        fatal "failed to get user of host $_host"
+        fatal "failed to get user of host $_host $_extra_msg"
     else
         echo $_user
     fi
@@ -351,16 +366,15 @@ do_sync() {
     local _origin_id="$2"
     local _dup_id="$3"
     local _dir="$4"
-    local _ssh_key=$PEM_FILE
 
     local _host1=$(util_get_host $_origin_id "(origin)")
     if [ "$_host1" == "" ]; then exit 1; fi
     local _host2=$(util_get_host $_dup_id "(duplicate $_index)")
     if [ "$_host2" == "" ]; then exit 1; fi
 
-    local _user1=$(util_get_user $_host1 $_ssh_key "(origin)")
+    local _user1=$(util_get_user $_host1 "(origin)")
     if [ "$_user1" == "" ]; then exit 1; fi
-    local _user2=$(util_get_user $_host2 $_ssh_key "(duplicate $_index)")
+    local _user2=$(util_get_user $_host2 "(duplicate $_index)")
     if [ "$_user2" == "" ]; then exit 1; fi
 
     for ((i=0; i<1; i++))
@@ -368,27 +382,24 @@ do_sync() {
         inform "$_origin_id -> $_dup_id, dir:$_dir, try $i"
         inform "$_user1@$_host1 -> $_user2@$_host2 :$_dir"
 
-        local _has_rsync=$(ssh -o BatchMode=yes -o StrictHostKeyChecking=no -i $_ssh_key $_user1@$_host1 'which rsync')
+        local _has_rsync=$(ssh -o BatchMode=yes -o StrictHostKeyChecking=no $_user1@$_host1 'which rsync')
         if [ "$_has_rsync" != "" ] ; then
             # generate temporary SSH key
-            local _tmp_key=$(ssh -o BatchMode=yes -o StrictHostKeyChecking=no -i $_ssh_key $_user1@$_host1 'if [ -e ".ssh/afewmore_tmp.rsa" ]; then echo T; fi')
+            local _tmp_key=$(ssh -o BatchMode=yes -o StrictHostKeyChecking=no $_user1@$_host1 'if [ -e ".ssh/afewmore_tmp.rsa" ]; then echo T; fi')
             if [ "$_tmp_key" == "" ]; then
-                ssh -o BatchMode=yes -o StrictHostKeyChecking=no -i $_ssh_key $_user1@$_host1 'ssh-keygen -N "" -q -t rsa -f ./.ssh/afewmore_tmp.rsa'
+                ssh -o BatchMode=yes -o StrictHostKeyChecking=no $_user1@$_host1 'ssh-keygen -N "" -q -t rsa -f ./.ssh/afewmore_tmp.rsa'
             fi
-            local _tmp_pubkey=`ssh -o BatchMode=yes -o StrictHostKeyChecking=no -i $_ssh_key $_user1@$_host1 'cat /$HOME/.ssh/afewmore_tmp.rsa.pub'`
-            ssh -o BatchMode=yes -o StrictHostKeyChecking=no -i $_ssh_key $_user2@$_host2 "echo $_tmp_pubkey >> ./.ssh/authorized_keys"
-            ssh -o BatchMode=yes -o StrictHostKeyChecking=no -i $_ssh_key $_user1@$_host1 "rsync -q -avr --progress -e 'ssh -o BatchMode=yes -o StrictHostKeyChecking=no -i .ssh/afewmore_tmp.rsa' -d $_dir $_user2@$_host2:$_dir 2>/dev/null"
+            local _tmp_pubkey=`ssh -o BatchMode=yes -o StrictHostKeyChecking=no $_user1@$_host1 'cat /$HOME/.ssh/afewmore_tmp.rsa.pub'`
+            ssh -o BatchMode=yes -o StrictHostKeyChecking=no $_user2@$_host2 "echo $_tmp_pubkey >> ./.ssh/authorized_keys"
+            ssh -o BatchMode=yes -o StrictHostKeyChecking=no $_user1@$_host1 "rsync -q -avr --progress -e 'ssh -o BatchMode=yes -o StrictHostKeyChecking=no -i .ssh/afewmore_tmp.rsa' -d $_dir $_user2@$_host2:$_dir 2>/dev/null"
             if [ "$?" == "0" ]; then
                 exit 0
             fi
         else
             warning "origin instance ($_origin_id) doesn't support 'rsync'"
-            ssh -o BatchMode=yes -o StrictHostKeyChecking=no -i $_ssh_key $_user2@$_host2 "mkdir -p '$_dir'"
-            echo scp -o BatchMode=yes -o StrictHostKeyChecking=no -3 -r -i $_ssh_key "'$_user1@$_host1:${_dir}.*'" "'$_user2@$_host2:$_dir'" | sh
+            ssh -o BatchMode=yes -o StrictHostKeyChecking=no $_user2@$_host2 "mkdir -p '$_dir'"
+            echo scp -o BatchMode=yes -o StrictHostKeyChecking=no -3 -r "'$_user1@$_host1:${_dir}.*'" "'$_user2@$_host2:$_dir'" | sh
             exit 0
-            # if [ "$?" == "0" ]; then
-            #     exit 0
-            # fi
         fi
     done
 
@@ -405,21 +416,22 @@ do_check_done() {
 
 # TODO: fix index
 main() {
-    local _ssh_key=$PEM_FILE
+    # verification (must do in order)
+    inform "verifying parameters and environment..."
+    verify_instance "$INSTANCE_ID"
+    verify_copy_num "$COPY_NUM"
+    verify_aws
+    verify_ssh "$INSTANCE_ID"
+    verify_copy_dir "$INSTANCE_ID" "$COPY_DIR"
 
+    # create task file
+    inform "creating task..."
     task_create
     local _origin_id=$(task_read_origin)
     local _dir=$(task_read_dir)
 
-    # pre-creation verification
-    local _host=$(util_get_host $_origin_id "(origin)")
-    if [ "$_host" == "" ]; then exit 1; fi
-    (do_check_ready "$_origin_id" "(origin)")
-    if [ "$?" != "0" ]; then exit 1; fi
-    local _user=$(util_get_user $_host $_ssh_key "(origin)")
-    if [ "$_user" == "" ]; then exit 1; fi
-    verify_copy_dir "$_ssh_key" "$_origin_id" "$_user" "$_host" "$_dir"
-
+    # main loop
+    inform "Start main loop."
     while [ $(task_done) != "T" ] ;
     do
         # -> created
@@ -465,9 +477,7 @@ main() {
 
         inform '---- all:' $(task_read_total_num) 'done:' $(task_count 'done') '----'
     done
-
-    # done
-    TellUser "All done."
+    inform "All done."
 }
 
 ##### option parsing & verification #####
@@ -499,9 +509,6 @@ while true ; do
                 INSTANCE_ID="$1"
                 shift
             else
-                verify_instance "$INSTANCE_ID" "illegal instance id $INSTANCE_ID"
-                verify_copy_num "$COPY_NUM"
-                verify_aws
                 main
                 exit 0
             fi ;;
